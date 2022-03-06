afc4f6d86173f85164e14e81dee569d0c9feb73c
G1Thomas <thomasgenin69@gmail.com>
2022-03-06 20:28:24 +0100

    GameClient

--- source/client/game.c
+++ source/client/game.c
@@ -8,3 +8,31 @@
 #include <stdio.h>
 #include "game.h"
 
+
+/**
+ * @brief Rafraichissement de l'interface avec les données jeuClient c recue du serveur (RoudRestant, Budget, Mises, Décisions et Score)
+ * @param[in] jeuClient c Structure : informations de données de jeu du Client
+ */
+
+void RefreshInterface(jeuClient c) {
+    
+    int gladeBudget = c.Budget;
+    int gladeRoundRestant = c.RoundRestant;
+    int gladeScore = c.Score;
+
+    //TODO Implementation de l'interface -Refresh 
+}
+
+/**
+ * @brief Construction de la structure sendc a envoyer au serveur avec les données résultant des choix de l'utilisateur (Mises et Décisions)
+ * @param[in] jeuClient c           Structure : informations de données de jeu du Client
+ * @param[in] int gladeMise         Int : mise de l'utilisateur 
+ * @param[in] int gladeDécision     Int : décision de l'utilisateur 
+ */
+
+jeuClient BuildStruct(jeuClient sendc, int gladeMise, int gladeDecision) {
+    sendc.Décision = gladeDecision;
+    sendc.Mise = gladeMise;
+
+    return sendc;
+}
--- source/client/game.h
+++ source/client/game.h
@@ -3,3 +3,6 @@
 #define GAME_H
 
 #endif /* GAME_H */
+
+void RefreshInterface(jeuClient);
+jeuClient BuildStruct(jeuClient sendc, int gladeMise, int gladeDecision);
