455704cb4c1761d7da4f932c12271217e1fc1a42
Maxime <maxime.benucci@cpe.fr>
2022-03-07 08:21:47 -0500

    feat: CSV writting

--- source/common/cfg.h
+++ source/common/cfg.h
@@ -17,7 +17,6 @@ typedef struct {
     int room_client_2;
 } cfgServer;
 
-
 /**
 * @brief Structure : informations de configuration du Client
 *
@@ -31,11 +30,38 @@ typedef struct {
     int id_Client;
 } cfgClient;
 
+/**
+* @brief Structure : informations de la partie en cours (Elles servent a ecrire le fichier de resultat)
+*
+* @param numeroRound
+* @param JeuJ1
+* @param JeuJ2
+* @param MiseDeJ1
+* @param MiseDeJ2
+* @param Score1
+* @param Score2
+*/
+typedef struct {
+    int numeroRound;
+    char* JeuJ1;
+    char* JeuJ2;
+    int MiseDeJ1;
+    int MiseDeJ2;
+    int Score1;
+    int Score2;
+}Resultat;
+
+
+void printEnTete(FILE *file);
+void printLine(FILE *file,Resultat res);
+void deroulementDuJeu();
+void ecritureDesResultats();
 
 cfgClient initCfgCli();
 cfgServer initCfgSrv();
 
 void afficherConfigCli(cfgClient cfg);
 void afficherConfigSrv(cfgServer cfg);
+
 
 #endif /* CFG.H */
